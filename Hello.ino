72a1fb4892093398bc7c0610b93fcd06902068b8
Angie Eraso <angieeraso10@gmail.com>
2025-02-21 10:31:59 -0500

    Create Hello.ino arduino sketch

---
+++ Hello.ino
@@ -0,0 +1,14 @@
+//Sketch Hello.ino
+
+int const LED = 13
+void setup() {
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);  //Baudios
+  digital.whrite(LEDm, LOW);
+  Serial.print("Hello");
+
+}
+
+void loop (){
+
+}
